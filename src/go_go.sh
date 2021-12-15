190efb093ed274ad7104bf831638d01202258e1a
GH0STAV0 <you@example.com>
2021-12-15 16:24:09 +0000

    add new project

--- src/go_go.sh
+++ src/go_go.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 clear
 trap "echo oh;exit" SIGTERM SIGINT
-# echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" >  /etc/resolv.conf
+echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" >  /etc/resolv.conf
 cd /root/SDA_ALL/site_1/
 
 while true
